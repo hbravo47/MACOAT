c175f7219837957bb32a012c7f045bbb687961f2
eclawson <eclawson@valdosta.edu>
2015-12-08 21:37:21 -0500

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,5 @@ module ApplicationHelper
 
   def print_Mac
     (image_tag("missing.png"))
-    	#{}"<span class='c1'>M</span><span class='c2'>O</span><span class='c3'>A</span><span class='c4'>T</span>")
   end
 end
